d9398ba932c80086d38c0f339ca0500e04fb8076
Frank Francis <barc30881@gmail.com>
2025-09-26 10:48:42 -0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
- "host"=> 'localhost',
- 'port'=> 3306,
- 'dbname'=> 'homehaven',
- 'username'=> 'ifeanyi',
- 'password'=> '123456'
+ "host"=> '',
+ 'port'=> ,
+ 'dbname'=> '',
+ 'username'=> '',
+ 'password'=> ''
 ];
